4dde447907d04b0f554b76128934491b19160cc2
Gregory Hapgood <greghap@gmail.com>
2022-01-29 11:48:23 -0600

    OhMyPosh, Powershell, winget files

---
+++ PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+[string]$PROFILE_DIRECTORY = Split-Path $PROFILE
+oh-my-posh --init --shell pwsh --config "${profile_directory}\oh-my-posh-config.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
---
+++ winget-install.ps1
@@ -0,0 +1,22 @@
+$PackageIds = @(
+    "Microsoft.PowerShell",
+    "Microsoft.WindowsTerminal",
+    "JanDeDobbeleer.OhMyPosh",
+    "Microsoft.VisualStudioCode",
+    "Python.Python.3",
+    "Microsoft.dotnet",
+    "Rustlang.Rust.GNU",
+    "OpenJS.NodeJS",
+    "Git.Git",
+    "Valve.Steam",
+    "Spotify.Spotify",
+    "7zip.7zip",
+    "flux.flux",
+    "Malwarebytes.Malwarebytes",
+    "GitHub.cli"
+)
+
+foreach ($PackageId in $PackageIds)
+{
+    winget install -e --id $PackageId -h
+}
