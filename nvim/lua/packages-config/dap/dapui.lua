1bd2752d86fbc6fdc9e7c34bc5776ff009df2350
Gregory Hapgood <greghap@gmail.com>
2022-05-14 09:13:08 -0500

    Updating dap icons

--- nvim/lua/packages-config/dap/dapui.lua
+++ nvim/lua/packages-config/dap/dapui.lua
@@ -1,12 +1,14 @@
-local nnoremap = require('util').nnoremap
-
 require('dapui').setup{}
 
+local nnoremap = require('util').nnoremap
 nnoremap('<F5>', ":lua require('dap').continue()<CR>")
 nnoremap('<F10>', ":lua require('dap').step_over()<CR>")
 nnoremap('<F11>', ":lua require('dap').step_into()<CR>")
 nnoremap('<Leader>db', ":lua require('dap').toggle_breakpoint()<CR>")
-nnoremap('<Leader>dB', ":lua require('dap').toggle_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+nnoremap('<Leader>dB', ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 nnoremap('<Leader>dw', ":lua require('dap').terminate()<CR>")
 nnoremap('<Leader>dui', ":lua require('dapui').toggle()<CR>")
 
+vim.fn.sign_define('DapBreakpoint', { text = '', texthl='Error', linehl='', numhl='' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl='Error', linehl='', numhl='' })
+vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl='Error', linehl='', numhl='' })
