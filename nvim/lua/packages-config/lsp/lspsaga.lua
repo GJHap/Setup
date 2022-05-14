9ee435c6bcdab2db936eafeeb162acb6ad3a78a1
Gregory Hapgood <greghap@gmail.com>
2022-05-14 09:14:09 -0500

    Keybind for range_code_action

--- nvim/lua/packages-config/lsp/lspsaga.lua
+++ nvim/lua/packages-config/lsp/lspsaga.lua
@@ -1,7 +1,8 @@
 local nnoremap = require('util').nnoremap
+local vnoremap = require('util').vnoremap
 
 require('lspsaga').setup{}
 
 nnoremap('<Leader>ca', ':Lspsaga code_action<CR>')
 nnoremap('<Leader>rr', ':Lspsaga rename<CR>')
-
+vnoremap('<Leader>ca', ":<c-u>Lspsaga range_code_action<cr>")
