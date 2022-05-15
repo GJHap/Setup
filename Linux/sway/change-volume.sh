c5a33cb34efdec6b0d3f0dd0bcfb6a43f69dd56a
Gregory Hapgood <greghap@gmail.com>
2022-05-14 20:28:29 -0500

    Updating volume notification

--- Linux/sway/change-volume.sh
+++ Linux/sway/change-volume.sh
@@ -4,14 +4,12 @@ pamixer $@
 
 volume=$(pamixer --get-volume)
 is_mute=$(pamixer --get-mute)
-icon=''
-rise='4pt'
+icon='audio-on'
 
 if [ $is_mute == true ] || [ $volume == 0 ]; then
-   icon='婢'
-   rise='6.75pt'
+   icon='audio-off'
 fi
 
 bar=$(seq -s "━" $(($volume / 5)) | sed 's/[0-9]//g')
-body="<span size='200%'>$icon</span>    <span rise='$rise' weight='heavy'>$bar</span>"
-dunstify "" "$body" -u low -a "change-volume" -h string:x-dunst-stack-tag:audio
+
+dunstify -a "change-volume" -h string:x-dunst-stack-tag:audio -i $icon -u low "" "<span>$bar</span>"
--- Linux/system-setup.sh
+++ Linux/system-setup.sh
@@ -3,6 +3,7 @@
 ./pacman-install.sh
 cp -r greetd/* /etc/greetd
 cp -r kanshi /etc/greetd/
+cp dunst/*.png /usr/share/icons
 
 ./rust-install.sh
 ./systemd-enable.sh
