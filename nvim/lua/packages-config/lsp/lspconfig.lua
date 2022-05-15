d464c41c5548f4101e6ae1ef3efc826b74cc21cb
Gregory Hapgood <greghap@gmail.com>
2022-05-15 12:53:42 -0500

    Only show diagnostics on demand

--- nvim/lua/packages-config/lsp/lspconfig.lua
+++ nvim/lua/packages-config/lsp/lspconfig.lua
@@ -1,11 +1,21 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local on_attach = function()
+   vim.diagnostic.config(
+   {
+      virtual_text = false,
+      update_in_insert = true
+   })
+end
 
 local lspconfig = require('lspconfig')
-
 servers = { 'pyright', 'tsserver', 'eslint', 'cssls', 'html' }
 for _,server in pairs(servers) do
    lspconfig[server].setup
    {
-      capabilities = capabilities
-   }  
+      capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      on_attach = on_attach
+   }
 end
+
+local nnoremap = require('util').nnoremap
+nnoremap('<Leader>df', ':lua vim.diagnostic.open_float() <CR>')
+
