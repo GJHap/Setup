1742a95f97da27d47a33353bcf992ad88578c766
Gregory Hapgood <greghap@gmail.com>
2022-05-17 19:42:33 -0500

    Updating volume notification

--- Linux/sway/change-volume.sh
+++ Linux/sway/change-volume.sh
@@ -3,6 +3,13 @@
 pamixer $@
 
 volume=$(pamixer --get-volume)
+volume_bar_character_count=$((volume / 5))
+volume_bar=$(printf "%${volume_bar_character_count}s" | sed 's/ /━/g')
+
+remaining_volume=$((100-$volume))
+remaining_volume_bar_character_count=$((remaining_volume / 5))
+remaining_volume_bar=$(printf "%${remaining_volume_bar_character_count}s" | sed 's/ /━/g')
+
 is_mute=$(pamixer --get-mute)
 icon='audio-on'
 
@@ -10,6 +17,4 @@ if [ $is_mute == true ] || [ $volume == 0 ]; then
    icon='audio-off'
 fi
 
-bar=$(seq -s "━" $(($volume / 5)) | sed 's/[0-9]//g')
-
-dunstify -a "change-volume" -h string:x-dunst-stack-tag:audio -i $icon -u low "" "<span>$bar</span>"
+dunstify -a "change-volume" -h string:x-dunst-stack-tag:audio -i $icon -u low "" "<span color='#FFFFFF'>$volume_bar</span><span color='gray'>$remaining_volume_bar</span>"
