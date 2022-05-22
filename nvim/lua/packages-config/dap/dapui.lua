96b0a1c0be8cbe31677372637a921d1554fa5581
Gregory Hapgood <greghap@gmail.com>
2022-05-22 10:46:44 -0500

    Adding keybindings for hover, refactoring to lua functions

--- nvim/lua/packages-config/dap/dapui.lua
+++ nvim/lua/packages-config/dap/dapui.lua
@@ -1,20 +1,28 @@
-require('dapui').setup{}
+local dapui = require('dapui')
+dapui.setup{}
 
-local nnoremap = require('util').nnoremap
-nnoremap('<F5>', ":lua require('dap').continue() <CR>")
-nnoremap('<F10>', ":lua require('dap').step_over() <CR>")
-nnoremap('<F11>', ":lua require('dap').step_into() <CR>")
-nnoremap('<Leader>db', ":lua require('dap').toggle_breakpoint() <CR>")
-nnoremap('<Leader>dB', ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) <CR>")
-nnoremap('<Leader>dq', ":lua require('dap').terminate() <CR>")
-nnoremap('<Leader>dee', ":lua require('dap').set_exception_breakpoints({ 'all' }) <CR>")
-nnoremap('<Leader>ded', ":lua require('dap').set_exception_breakpoints({}) <CR>")
-nnoremap('<Leader>dui', ":lua require('dapui').toggle() <CR>")
-nnoremap('<Leader>ds', ":lua require('dapui').float_element('stacks', { enter = true }) <CR>")
-nnoremap('<Leader>dw', ":lua require('dapui').float_element('watches', { enter = true }) <CR>")
-nnoremap('<Leader>dl', ":lua require('dapui').float_element('scopes', { enter = true }) <CR>")
-nnoremap('<Leader>dp', ":lua require('dapui').float_element('breakpoints', { enter = true }) <CR>")
-nnoremap('<Leader>dr', ":lua require('dapui').float_element('repl', { enter = true }) <CR>")
+local dap = require('dap')
+
+local util = require('util')
+local nnoremap = util.nnoremap
+local vnoremap = util.vnoremap
+
+nnoremap('<F5>', function() dap.continue() end)
+nnoremap('<F10>', function() dap.step_over() end)
+nnoremap('<F11>', function() dap.step_into() end)
+nnoremap('<Leader>db', function() dap.toggle_breakpoint() end)
+nnoremap('<Leader>dB', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+nnoremap('<Leader>dq', function() dap.terminate() end)
+nnoremap('<Leader>dee', function() dap.set_exception_breakpoints({ 'all' }) end)
+nnoremap('<Leader>ded', function() dap.set_exception_breakpoints({}) end)
+nnoremap('<Leader>dui', function() dapui.toggle() end)
+nnoremap('<Leader>ds', function() dapui.float_element('stacks', { enter = true }) end)
+nnoremap('<Leader>dw', function() dapui.float_element('watches', { enter = true }) end)
+nnoremap('<Leader>dl', function() dapui.float_element('scopes', { enter = true }) end)
+nnoremap('<Leader>dp', function() dapui.float_element('breakpoints', { enter = true }) end)
+nnoremap('<Leader>dr', function() dapui.float_element('repl', { enter = true }) end)
+nnoremap('<Leader>dh', function() dapui.eval() end)
+vnoremap('<Leader>dh', function() dapui.eval() end)
 
 vim.fn.sign_define('DapBreakpoint', { text = '', texthl='Error', linehl='', numhl='' })
 vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl='Error', linehl='', numhl='' })
