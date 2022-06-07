c4a416a86e8e2bf8eb16cf968b45fb3b7ee1d3fe
Gregory Hapgood <greghap@gmail.com>
2022-06-06 19:16:36 -0500

    global statusline + winbar

--- nvim/lua/packages-config/feline.lua
+++ nvim/lua/packages-config/feline.lua
@@ -9,6 +9,7 @@ prequire('feline', function(feline)
    feline.setup
    {
       theme = theme,
+      force_inactive = {},
       components =
       {
          active =
@@ -35,6 +36,15 @@ prequire('feline', function(feline)
                   hl =
                   {
                      bg = 'blue0'
+                  }
+               },
+               {
+                  provider = "git_branch",
+                  left_sep = "slant_left",
+                  right_sep = "slant_right_2",
+                  hl =
+                  {
+                     bg = 'dark3'
                   }
                },
                {
@@ -98,26 +108,84 @@ prequire('feline', function(feline)
                },
             }
          },
-         inactive =
-         {
-            {
-               {
-                  provider = 'file_info',
-                  left_sep = 'slant_left',
-                  right_sep = 'slant_right_2',
-                  opts =
-                  {
-                     type = 'base-only'
-                  },
-                  hl =
-                  {
-                     bg = 'blue0'
-                  }
-               }
-            },
-            {
-            }
-         }
       }
    }
+
+   feline.winbar.setup(
+   {
+      theme = theme,
+      conditional_components =
+      {
+         {
+            condition = function()
+               local function is_window_float(window)
+                  local cfg = vim.api.nvim_win_get_config(window)
+                  return cfg.relative > "" or cfg.external
+               end
+
+               local function is_window_invalid_filetype(window)
+                  local ft = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(window), "filetype")
+
+                  return ft:match('^NvimTree$')
+                     or ft:match('^packer$')
+                     or ft:match('^fugitive$')
+                     or ft:match('^fugitiveblame$')
+                     or ft:match('^help$')
+                     or ft:match('^toggleterm$')
+               end
+
+               local windows = vim.api.nvim_tabpage_list_wins(vim.api.nvim_get_current_tabpage())
+               local window_count = 0
+
+               for _, window in pairs(windows) do
+                  if not is_window_float(window) and not is_window_invalid_filetype(window) then
+                     window_count = window_count + 1
+                  end
+               end
+               return window_count > 1
+            end,
+            active =
+            {
+               {
+                  {
+                     provider = 'file_info',
+                     left_sep = 'slant_left',
+                     right_sep = 'slant_right_2',
+                     opts =
+                     {
+                        type = 'base-only'
+                     },
+                     hl =
+                     {
+                        bg = 'blue0'
+                     }
+                  }
+               },
+            },
+            inactive =
+            {
+               {
+                  {
+                     provider = 'file_info',
+                     left_sep = 'slant_left',
+                     right_sep = 'slant_right_2',
+                     opts =
+                     {
+                        type = 'base-only'
+                     },
+                     hl =
+                     {
+                        bg = 'blue0'
+                     }
+                  }
+               },
+            }
+         },
+      },
+      components =
+      {
+         active = {},
+         inactive = {}
+      }
+   })
 end)
