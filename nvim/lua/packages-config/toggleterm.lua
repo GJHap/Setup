2723f82b4b187ca49f66982be4faea8dbf4f0610
Gregory Hapgood <greghap@gmail.com>
2022-06-24 21:30:39 -0500

    Toggle term => Each tab gets it's own terminal

--- nvim/lua/packages-config/toggleterm.lua
+++ nvim/lua/packages-config/toggleterm.lua
@@ -1,14 +1,20 @@
 local util = require('util')
 local prequire = util.prequire
 local tnoremap = util.tnoremap
+local nnoremap = util.nnoremap
 
 prequire('toggleterm', function(toggleterm)
    toggleterm.setup({
-      size = 10,
-      open_mapping = '<C-\\>',
-      direction = 'horizontal',
       shade_terminals = false,
    })
 
+   nnoremap('<C-\\>', function()
+      local tab = vim.api.nvim_get_current_tabpage()
+      toggleterm.toggle(tab)
+   end)
+   tnoremap('<C-\\>', function()
+      local tab = vim.api.nvim_get_current_tabpage()
+      toggleterm.toggle(tab)
+   end)
    tnoremap('<esc>', [[<C-\><C-n>]])
 end)
