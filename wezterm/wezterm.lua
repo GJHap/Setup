affd47d949417a283301bd71d2d3428e5c89c974
Gregory Hapgood <greghap@gmail.com>
2022-06-25 14:03:14 -0500

    Wez updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,31 +1,32 @@
 local wezterm = require('wezterm')
+local action = wezterm.action
 
 return {
    enable_wayland = true,
    font = wezterm.font('mononoki NF'),
    hide_tab_bar_if_only_one_tab = true,
-   colors = { background = '#1a1b26' },
+   color_scheme = 'tokyonight',
    leader = { key = ',', mods = 'CTRL' },
    keys = {
-      { key = 'c', mods = 'LEADER', action = wezterm.action({ SplitHorizontal = { domain = 'CurrentPaneDomain' } }) },
-      { key = 'r', mods = 'LEADER', action = wezterm.action({ SplitVertical = { domain = 'CurrentPaneDomain' } }) },
+      { key = 'c', mods = 'LEADER', action = action.SplitHorizontal({ domain = 'CurrentPaneDomain' }) },
+      { key = 'r', mods = 'LEADER', action = action.SplitVertical({ domain = 'CurrentPaneDomain' }) },
       {
          key = 's',
          mods = 'LEADER',
-         action = wezterm.action({ ActivateKeyTable = { name = 'resize', one_shot = false } }),
+         action = action.ActivateKeyTable({ name = 'resize', one_shot = false, replace_current = true }),
       },
-      { key = 'h', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Left' }) },
-      { key = 'l', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Right' }) },
-      { key = 'k', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Up' }) },
-      { key = 'j', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Down' }) },
+      { key = 'h', mods = 'LEADER', action = action.ActivatePaneDirection('Left') },
+      { key = 'l', mods = 'LEADER', action = action.ActivatePaneDirection('Right') },
+      { key = 'k', mods = 'LEADER', action = action.ActivatePaneDirection('Up') },
+      { key = 'j', mods = 'LEADER', action = action.ActivatePaneDirection('Down') },
    },
    key_tables = {
       resize = {
-         { key = 'h', action = wezterm.action({ AdjustPaneSize = { 'Left', 5 } }) },
-         { key = 'j', action = wezterm.action({ AdjustPaneSize = { 'Down', 5 } }) },
-         { key = 'k', action = wezterm.action({ AdjustPaneSize = { 'Up', 5 } }) },
-         { key = 'l', action = wezterm.action({ AdjustPaneSize = { 'Right', 5 } }) },
-         { key = 'Escape', action = 'PopKeyTable' },
+         { key = 'h', action = action({ AdjustPaneSize = { 'Left', 5 } }) },
+         { key = 'j', action = action({ AdjustPaneSize = { 'Down', 5 } }) },
+         { key = 'k', action = action({ AdjustPaneSize = { 'Up', 5 } }) },
+         { key = 'l', action = action({ AdjustPaneSize = { 'Right', 5 } }) },
+         { key = 'Escape', action = action.PopKeyTable },
       },
    },
 }
