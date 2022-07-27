b25ad8947f43bd2950a3b3da8f4806d51304611f
GJHap <greghap@gmail.com>
2022-07-26 19:40:04 -0500

    nvim-surround

--- nvim/lua/packages-config/surround.lua
+++ nvim/lua/packages-config/surround.lua
@@ -1,10 +1,5 @@
 local prequire = require('util').prequire
 
-prequire('surround', function(surround)
-   surround.setup({
-      mapping_style = 'sandwich',
-      prefix = '<Leader>s',
-      prompt = false,
-      space_on_closing_char = true,
-   })
+prequire('nvim-surround', function(surround)
+   surround.setup({})
 end)
