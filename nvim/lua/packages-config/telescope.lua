d9dfae051190890a9a1fabc3a34100e2a1e0db94
GJHap <greghap@gmail.com>
2022-08-28 09:42:25 -0500

    Telescope workaround for multiselect

--- nvim/lua/packages-config/telescope.lua
+++ nvim/lua/packages-config/telescope.lua
@@ -5,16 +5,102 @@ local nnoremap = util.nnoremap
 prequire('telescope', function(telescope)
    local mappings = {}
    prequire('telescope.actions', function(actions)
+      local select_items = actions.select_default
+      prequire('telescope.actions.state', function(actions_state)
+         prequire('telescope.pickers', function(pickers)
+            prequire('plenary.path', function(plenary_path)
+               local function multiopen(prompt_bufnr, method)
+                  local edit_file_cmd_map = {
+                     vertical = 'vsplit',
+                     horizontal = 'split',
+                     tab = 'tabedit',
+                     default = 'edit',
+                  }
+                  local edit_buf_cmd_map = {
+                     vertical = 'vert sbuffer',
+                     horizontal = 'sbuffer',
+                     tab = 'tab sbuffer',
+                     default = 'buffer',
+                  }
+                  local picker = actions_state.get_current_picker(prompt_bufnr)
+                  local multi_selection = picker:get_multi_selection()
+
+                  if #multi_selection > 1 then
+                     pickers.on_close_prompt(prompt_bufnr)
+                     pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+                     for i, entry in ipairs(multi_selection) do
+                        local filename, row, col
+
+                        if entry.path or entry.filename then
+                           filename = entry.path or entry.filename
+
+                           row = entry.row or entry.lnum
+                           col = vim.F.if_nil(entry.col, 1)
+                        elseif not entry.bufnr then
+                           local value = entry.value
+                           if not value then
+                              return
+                           end
+
+                           if type(value) == 'table' then
+                              value = entry.display
+                           end
+
+                           local sections = vim.split(value, ':')
+
+                           filename = sections[1]
+                           row = tonumber(sections[2])
+                           col = tonumber(sections[3])
+                        end
+
+                        local entry_bufnr = entry.bufnr
+
+                        if entry_bufnr then
+                           if not vim.api.nvim_buf_get_option(entry_bufnr, 'buflisted') then
+                              vim.api.nvim_buf_set_option(entry_bufnr, 'buflisted', true)
+                           end
+                           local command = i == 1 and 'buffer' or edit_buf_cmd_map[method]
+                           pcall(vim.cmd, string.format('%s %s', command, vim.api.nvim_buf_get_name(entry_bufnr)))
+                        else
+                           local command = i == 1 and 'edit' or edit_file_cmd_map[method]
+                           if vim.api.nvim_buf_get_name(0) ~= filename or command ~= 'edit' then
+                              filename = plenary_path:new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+                              pcall(vim.cmd, string.format('%s %s', command, filename))
+                           end
+                        end
+
+                        if row and col then
+                           pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
+                        end
+                     end
+                  else
+                     actions['select_' .. method](prompt_bufnr)
+                  end
+               end
+
+               select_items = function(prompt_bufnr)
+                  multiopen(prompt_bufnr, 'default')
+               end
+            end)
+         end)
+      end)
       mappings = {
          n = {
             ['<Leader>sc'] = actions.select_vertical,
             ['<Leader>sr'] = actions.select_horizontal,
             ['<C-d>'] = actions.results_scrolling_down,
             ['<C-u>'] = actions.results_scrolling_up,
+            ['<CR>'] = select_items,
+            ['<Tab>'] = actions.toggle_selection + actions.move_selection_better,
+            ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_worse,
          },
          i = {
             ['<C-d>'] = actions.results_scrolling_down,
             ['<C-u>'] = actions.results_scrolling_up,
+            ['<CR>'] = select_items,
+            ['<Tab>'] = actions.toggle_selection + actions.move_selection_better,
+            ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_worse,
          },
       }
    end)
