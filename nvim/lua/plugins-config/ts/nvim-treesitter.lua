0c5199ca403dbb76ad39bc0bbc9ca3ddf6546902
GJHap <greghap@gmail.com>
2022-10-14 19:34:36 -0500

    treesitter auto_install

--- nvim/lua/plugins-config/ts/nvim-treesitter.lua
+++ nvim/lua/plugins-config/ts/nvim-treesitter.lua
@@ -2,6 +2,7 @@ local prequire = require('util').prequire
 
 prequire('nvim-treesitter.configs', function(treesitter)
    treesitter.setup({
+      auto_install = true,
       highlight = {
          enable = true,
       },
