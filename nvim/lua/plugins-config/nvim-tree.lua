3f6671212bd37229a3961c079da36888ce042c45
GJHap <greghap@gmail.com>
2022-11-23 16:52:06 -0600

    Fix: nvim-tree in tabbed environment

--- nvim/lua/plugins-config/nvim-tree.lua
+++ nvim/lua/plugins-config/nvim-tree.lua
@@ -2,7 +2,7 @@ local prequire = require('util').prequire
 
 prequire('nvim-tree', function(nvim_tree)
    nvim_tree.setup({
-      update_cwd = true,
+      respect_buf_cwd = true,
       view = {
          adaptive_size = true,
          mappings = {
