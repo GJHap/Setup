fae5cd99a01e592add12b0841dfced822c13b72f
GJHap <greghap@gmail.com>
2023-01-30 17:36:46 -0600

    Neovim: Refactor

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -5,25 +5,21 @@ local function set_keymap(mode, lhs, rhs, opts)
    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-local function nnoremap(lhs, rhs, opts)
+local M = {}
+M.nnoremap = function(lhs, rhs, opts)
    set_keymap('n', lhs, rhs, opts)
 end
 
-local function vnoremap(lhs, rhs, opts)
+M.vnoremap = function(lhs, rhs, opts)
    set_keymap('v', lhs, rhs, opts)
 end
 
-local function inoremap(lhs, rhs, opts)
+M.inoremap = function(lhs, rhs, opts)
    set_keymap('i', lhs, rhs, opts)
 end
 
-local function tnoremap(lhs, rhs, opts)
+M.tnoremap = function(lhs, rhs, opts)
    set_keymap('t', lhs, rhs, opts)
 end
 
-return {
-   nnoremap = nnoremap,
-   vnoremap = vnoremap,
-   inoremap = inoremap,
-   tnoremap = tnoremap,
-}
+return M
