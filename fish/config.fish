2ad788058feb5689ea1a3d68b7460a5daa660aeb
GJHap <greghap@gmail.com>
2023-02-06 19:54:03 -0600

    fish: disable greeting and customize mode prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,19 @@
 starship init fish | source
 fish_vi_key_bindings
+set fish_greeting
+
+function fish_mode_prompt
+  set_color --bold \#41a6b5
+  switch $fish_bind_mode
+    case default
+      echo -n 'N'
+    case insert
+      echo -n 'I'
+    case replace_one
+      echo 'R'
+    case visual
+      echo 'V'
+    case '*'
+      echo '?'
+  end
+end
