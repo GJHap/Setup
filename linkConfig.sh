2a510e82f81a8d13ba714011732aeafd59ce82d9
Gregory Hapgood <greghap@gmail.com>
2024-12-14 13:55:30 -0800

    Script to link config files to appropriate locations

---
+++ linkConfig.sh
@@ -0,0 +1,7 @@
+ln -s ~/dev/Config/fish ~/config/fish
+ln -s ~/dev/Config/lf ~/config/lf
+ln -s ~/dev/Config/nvim ~/config/nvim
+ln -s ~/dev/Config/tmux ~/config/tmux
+ln -s ~/dev/Config/wezterm ~/config/wezterm
+ln -s ~/dev/Config/zsh/.zshrc ~/.zshrc
+ln -s ~/dev/Config/zsh/zsh ~/zsh
